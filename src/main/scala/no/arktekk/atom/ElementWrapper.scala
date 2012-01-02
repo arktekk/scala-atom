d3acd19bc9bfce9d081500cce483c38bfe5bd82f
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-01-02 13:53:23 +0100

    Added licence

--- src/main/scala/no/arktekk/atom/ElementWrapper.scala
+++ src/main/scala/no/arktekk/atom/ElementWrapper.scala
@@ -1,15 +1,25 @@
+/*
+ * Copyright 2011 Arktekk AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package no.arktekk.atom
 
 import com.codecommit.antixml._
 
 /**
- * Created by IntelliJ IDEA.
- * User: maedhros
- * Date: 1/2/12
- * Time: 11:46 AM
- * To change this template use File | Settings | File Templates.
+ * @author Erlend Hamnaberg<erlend@hamnaberg.net>
  */
-
 abstract class ElementWrapper(elem: Elem) {
   def wrapped = elem
 
