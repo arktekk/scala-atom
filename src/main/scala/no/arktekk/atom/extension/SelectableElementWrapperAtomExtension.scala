e0370a3f43a412366b604b92f4e4027e91ef8499
Erlend Hamnaberg <erlend@hamnaberg.net>
2012-01-05 16:43:49 +0100

    Added Copyright and author

--- src/main/scala/no/arktekk/atom/extension/SelectableElementWrapperAtomExtension.scala
+++ src/main/scala/no/arktekk/atom/extension/SelectableElementWrapperAtomExtension.scala
@@ -1,16 +1,26 @@
+/*
+ * Copyright 2011 Arktekk AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package no.arktekk.atom.extension
 
 import com.codecommit.antixml.{Elem, Selector}
 import no.arktekk.atom.{ElementWrapper}
 
 /**
- * Created by IntelliJ IDEA.
- * User: maedhros
- * Date: 1/5/12
- * Time: 12:08 PM
- * To change this template use File | Settings | File Templates.
+ * @author Erlend Hamnaberg<erlend@hamnaberg.net>
  */
-
 trait SelectableElementWrapperAtomExtension[A <: ElementWrapper, B] extends AtomExtension[A, B] {
   def selector: Selector[Elem]
   def function: (Elem) => B
--- src/main/scala/no/arktekk/atom/extension/opensearch/ItemsPerPageAtomExtension.scala
+++ src/main/scala/no/arktekk/atom/extension/opensearch/ItemsPerPageAtomExtension.scala
@@ -1,12 +1,22 @@
+/*
+ * Copyright 2011 Arktekk AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package no.arktekk.atom.extension.opensearch
 
 
 /**
- * Created by IntelliJ IDEA.
- * User: maedhros
- * Date: 1/5/12
- * Time: 11:01 AM
- * To change this template use File | Settings | File Templates.
+ * @author Erlend Hamnaberg<erlend@hamnaberg.net>
  */
-
 object ItemsPerPageAtomExtension extends IntAtomExtension("itemsPerPage")
